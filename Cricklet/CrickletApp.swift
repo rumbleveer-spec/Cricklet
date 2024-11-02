c50ff338c038d4058da25a1ebad324b781efa834
hm-arora <himanshuarora3097@gmail.com>
2024-11-02 23:02:41 +0530

    Initial Commit

---
+++ Cricklet/CrickletApp.swift
@@ -0,0 +1,17 @@
+//
+//  CrickletApp.swift
+//  Cricklet
+//
+//  Created by Himanshu on 02/11/24.
+//
+
+import SwiftUI
+
+@main
+struct CrickletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
